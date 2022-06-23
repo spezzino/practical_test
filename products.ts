e2fff75efc49367f0db7bb551fba5fb191bf5188
Stefano Pezzino <2013367+spezzino@users.noreply.github.com>
2022-06-23 14:06:36 +1200

    Initialize project using Create React App

---
+++ products.ts
@@ -0,0 +1,21 @@
+const products = [
+    {
+        name: "Sledgehammer",
+        price: 125.75
+    },
+    {
+        name: "Axe",
+        price: 190.50
+    },
+    {
+        name: "Bandsaw",
+        price: 562.13
+    }, {
+        name: "Chisel"
+        price: 12.9
+    },
+    {
+        name: "Hacksaw",
+        price: 18.45
+    }
+]
